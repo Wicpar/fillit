9efea41397c3874f98d7da77d123633781257545
Frederic NIETO <fnieto@e3r4p9.42.fr>
2015-12-10 04:17:41 +0100

    done lodar.c

--- loader.c
+++ loader.c
@@ -6,7 +6,7 @@
 /*   By: fnieto <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 20:37:02 by fnieto            #+#    #+#             */
-/*   Updated: 2015/12/10 04:15:52 by fnieto           ###   ########.fr       */
+/*   Updated: 2015/12/10 04:17:29 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,30 +27,14 @@ t_list	*loadfile(char	*str)
 	buf = ft_strnew(21);
 	if (fd < 0)
 		puterr(1);
-
-	ft_putstr("done loading\n");
-
 	while((last = read(fd, buf, 21)) >= 20)
 	{
-		ft_putendl("starting");
-
 		tmp = modelnew(ft_strsplit(buf, '\n'));
-
-		ft_putendl("done malloc");
-
 		tmp = check_model(tmp);
-
-		ft_putendl("checked model");
-
 		ft_lstpush(&lst, ft_lstnew(tmp, 1));
-
-		ft_putendl("added model");
-		ft_putendl(buf);
 		if (last != 20)
 			ft_strclr(buf);
 	}
-	ft_putnbr(last);
-
 	if (ft_strlen(buf) != 20 || close(fd) || last || !lst)
 		puterr(1);
 	ft_memdel((void**)&buf);
